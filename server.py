12e396d6d76c0743eef42cfa1e21bba0dad5aafa
Matthew Glazar <strager.nds@gmail.com>
2017-03-25 11:42:27 -0700

    Initial commit

---
+++ server.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python3.4
+
+import abc
+import html
+import http.server
+import io
+import json
+import logging
+import time
+
+logger = logging.getLogger(__name__)
+
+class HTMLPagelet(object, metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def write_loaded_content(self, buffer):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def is_content_loaded(self):
+        raise NotImplementedError()
+
+    def placeholder_id(self, placeholder_index):
+        return '__HTMLPagelet_placeholder_{}'.format(placeholder_index)
+
+    def encoding(self):
+        return 'utf-8'
+
+    def write_in_place(self, buffer, placeholder_index_factory):
+        if not self.is_content_loaded():
+            self.write_placeholder(buffer, placeholder_index=placeholder_index_factory(self))
+            return [self]
+        self.write_loaded_content(buffer)
+        return []
+
+    def write_placeholder(self, buffer, placeholder_index):
+        placeholder_id = self.placeholder_id(placeholder_index)
+        # TODO(strager): Make inline (<span>) versus block (<div>) configurable.
+        buffer.write('<div style="display:none;" id={placeholder_id}></div>'.format(
+            placeholder_id=html.escape(placeholder_id, quote=True),
+        ).encode(self.encoding()))
+        return [self]
+
+    def write_fixup(self, buffer, placeholder_index, placeholder_index_factory):
+        encoding = self.encoding()
+        with io.BytesIO() as temp_buffer:
+            pending_pagelets = self.write_in_place(temp_buffer, placeholder_index_factory=placeholder_index_factory)
+            js = '''(function () {{
+var placeholder = document.getElementById({placeholder_id});
+var html = {html};
+
+var placeholderParent = placeholder.parentNode;
+var replacement;
+var container;
+switch (placeholderParent.nodeType) {{
+    case Node.ELEMENT_NODE:
+        container = document.createElement(placeholderParent.nodeName);
+        container.innerHTML = html;
+        replacement = document.createDocumentFragment();
+        while (container.firstChild) {{
+            replacement.appendChild(container.removeChild(container.firstChild));
+        }}
+        break;
+    default:
+        throw Exception('Not implemented')
+}}
+placeholderParent.replaceChild(replacement, placeholder);
+}}());'''.format(
+                html=json.dumps(temp_buffer.getvalue().decode(encoding)),
+                placeholder_id=json.dumps(self.placeholder_id(placeholder_index)),
+            )
+        # FIXME(strager): We should HTML-escape.
+        buffer.write('<script>{js}</script>'.format(js=js).encode(encoding))
+        return pending_pagelets
+
+class LiteralHTMLPagelet(HTMLPagelet):
+    def __init__(self, html):
+        super(LiteralHTMLPagelet, self).__init__()
+        self.__html = html
+
+    def write_loaded_content(self, buffer):
+        buffer.write(str.encode(self.__html, self.encoding()))
+
+    def is_content_loaded(self):
+        return True
+
+class TriggeredHTMLPagelet(HTMLPagelet):
+    def __init__(self):
+        super(TriggeredHTMLPagelet, self).__init__()
+        self.__html = None
+
+    def set_html(self, html):
+        if html is None:
+            raise ValueError()
+        self.__html = html
+
+    def write_loaded_content(self, buffer):
+        assert self.__html is not None
+        buffer.write(str.encode(self.__html, self.encoding()))
+
+    def is_content_loaded(self):
+        return self.__html is not None
+
+class PageletWriter(object):
+    def __init__(self):
+        self.__placeheld_pagelets = set()
+        self.__next_pagelet_placeholder_index = 0
+        self.__pagelet_placeholder_indexes = {}
+
+    def __pagelet_placeholder_index(self, pagelet):
+        placeholder_index = self.__pagelet_placeholder_indexes.get(pagelet)
+        if placeholder_index is not None:
+            return placeholder_index
+        placeholder_index = self.__next_pagelet_placeholder_index
+        self.__next_pagelet_placeholder_index += 1
+        self.__pagelet_placeholder_indexes[pagelet] = placeholder_index
+        return placeholder_index
+
+    def write_pagelet_in_place(self, pagelet, buffer):
+        pending_pagelets = pagelet.write_in_place(buffer, placeholder_index_factory=self.__pagelet_placeholder_index)
+        for pagelet in pending_pagelets:
+            self.__placeheld_pagelets.add(pagelet)
+
+    def write_fixups(self, buffer):
+        for pagelet in list(self.__placeheld_pagelets):
+            if pagelet.is_content_loaded():
+                placeholder_index = self.__pagelet_placeholder_indexes[pagelet]
+                pending_pagelets = pagelet.write_fixup(buffer, placeholder_index=placeholder_index, placeholder_index_factory=self.__pagelet_placeholder_index)
+                self.__placeheld_pagelets.remove(pagelet)
+                for pagelet in pending_pagelets:
+                    self.__placeheld_pagelets.add(pagelet)
+
+    def has_pending_fixups(self):
+        return bool(self.__placeheld_pagelets)
+
+class RequestHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+
+        pagelet_writer = PageletWriter()
+        encoding = 'utf-8'
+        buffer = self.wfile
+        buffer.write('''<!DOCTYPE html>
+<html>
+<head>
+<title>Pagelets example</title>
+<meta charset=utf-8>
+</head>
+<body>
+<h1>Pagelets example</h1>
+<p>This page demonstrates pagelets.
+'''.encode(encoding))
+
+        for _ in range(100):
+            # HACK(strager): Some browsers like Firefox have
+            # a minimum number of bytes before progressive
+            # rendering kicks in.
+            buffer.write('<span></span>'.encode(encoding))
+
+        pagelet_a = LiteralHTMLPagelet('''<h2>Pagelet A</h2>
+<p>This is pagelet A.''')
+        pagelet_writer.write_pagelet_in_place(pagelet_a, buffer)
+
+        pagelet_b = TriggeredHTMLPagelet()
+        pagelet_writer.write_pagelet_in_place(pagelet_b, buffer)
+
+        pagelet_c = LiteralHTMLPagelet('''<h2>Pagelet C</h2>
+<p>This is pagelet C.''')
+        pagelet_writer.write_pagelet_in_place(pagelet_c, buffer)
+
+        buffer.flush()
+        time.sleep(3)
+
+        pagelet_b.set_html('''<h2>Pagelet B</h2>
+<p>This is pagelet B.''')
+        pagelet_writer.write_fixups(buffer)
+        if pagelet_writer.has_pending_fixups():
+            raise Exception('Fixups not all resolved')
+
+        self.wfile.write('''</body>
+</html>
+'''.encode(encoding))
+
+def main():
+    address = ('', 8000)
+    http_server = http.server.HTTPServer(address, RequestHandler)
+    logger.info('Serving on address: %s:%d', *http_server.server_address)
+    http_server.serve_forever()
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    main()
